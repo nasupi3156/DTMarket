8b77ffc92f8288a989a287af4fde7e7baec07a51
nasupi <nasubi54kk@gmail.com>
2024-09-27 21:29:12 +0900

    Created EC-site

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+namespace shopping;
+
+
+require_once dirname(__FILE__) . '/shopping/Bootstrap.class.php';
+
+use shopping\Bootstrap;
+
+
+$loader = new \Twig\Loader\FilesystemLoader(Bootstrap::TEMPLATE_DIR);
+$twig = new \Twig\Environment($loader, [
+'cache' => Bootstrap::CACHE_DIR,
+]);
+
+
+header("Location: ../shopping/list.php");
+
+exit();
+
+$context = [];
+$template = $twig->loadTemplate('index.html.twig');
+$template->display($context);
