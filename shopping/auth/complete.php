75a7da3ebb4c5c6e77f024c2be33b1c34afaa5ef
nasupi <nasubi54kk@gmail.com>
2024-10-03 02:43:27 +0900

    Move PHP files to auth folder

---
+++ shopping/auth/complete.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace shopping;
+
+require_once dirname(__FILE__) .'/../Bootstrap.class.php';
+
+use shopping\Bootstrap;
+use shopping\lib\PDODatabase;
+use shopping\lib\Session;
+
+$db = new PDODatabase(Bootstrap::DB_HOST, Bootstrap::DB_USER, Bootstrap::DB_PASS, Bootstrap::DB_NAME, Bootstrap::DB_TYPE);
+$ses = new Session($db);
+
+$loader = new \Twig\Loader\FilesystemLoader(Bootstrap :: TEMPLATE_DIR);
+$twig = new \Twig\Environment($loader, [
+'cache' => Bootstrap :: CACHE_DIR
+]);
+
+$context = [];
+
+$template = $twig->loadTemplate('complete.html.twig');
+$template->display($context);
+
+
+
+
---
+++ shopping/auth/postcode_search.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace shopping;
+
+require_once dirname(__FILE__). '/../Bootstrap.class.php';
+
+use shopping\lib\PDODatabase;
+use shopping\Bootstrap;
+
+$db = new PDODatabase(Bootstrap::DB_HOST, Bootstrap::DB_USER, Bootstrap::DB_PASS, Bootstrap::DB_NAME, Bootstrap::DB_TYPE);
+
+if (isset($_GET['zip1']) === true && isset($_GET['zip2']) === true){
+
+  $zip1 = trim($_GET['zip1']);
+  $zip2 = trim($_GET['zip2']);
+  // trim ： ユーザーが入力した余分な空白などを削除
+  
+  $res = $db -> getPostcodeSearch($zip1. $zip2); // 郵便番号を結合
+
+  if ($res) {
+    echo ($res !== '' && count($res) !== 0) ? $res['pref']. $res['city'].$res['town']: false;
+ 
+  } else {
+    echo 'no';
+  }
+}
